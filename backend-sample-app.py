1ec8fd8d11d6d70f4e1d8c17ea56eeac69da299b
Paulo Mariano <pazevedo@stone.com.br>
2018-02-14 16:37:29 -0200

    imports codecs

--- backend-sample-app.py
+++ backend-sample-app.py
@@ -9,7 +9,7 @@
 # 1) accepts GET  requests on /login and responds with a login form
 # 2) accepts POST requests on /login, sets a cookie, and responds with redirect
 
-import sys, os, signal, base64, Cookie, cgi, urlparse
+import sys, os, signal, base64, Cookie, cgi, urlparse, codecs
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 Listen = ('localhost', 9000)
@@ -46,28 +46,8 @@ class AppHandler(BaseHTTPRequestHandler):
             self.send_response(500)
             return
 
-        html="""
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-  <head>
-    <meta http-equiv=Content-Type content="text/html;charset=UTF-8">
-    <title>Auth form example</title>
-  </head>
-  <body>
-    <form action="/login" method="post">
-      <table>
-        <tr>
-          <td>Username: <input type="text" name="username"/></td>
-        <tr>
-          <td>Password: <input type="text" name="password"/></td>
-        <tr>
-          <td><input type="submit" value="Login"></td>
-      </table>
-        <input type="hidden" name="target" value="TARGET">
-    </form>
-  </body>
-</html>"""
-
+        html_file = codecs.open("login-page/login.html", 'r', 'utf-8')
+        html = html_file.read()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(html.replace('TARGET', target))
